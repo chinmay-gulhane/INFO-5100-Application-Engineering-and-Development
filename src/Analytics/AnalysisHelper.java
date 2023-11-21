5cb30a1ea8af51544ab134d8b0305e16d72b375f
Chinmay Gulhane <chinmay.gulhane@gmail.com>
2023-11-20 23:06:01 -0500

    Changes for Review comission panel

--- src/Analytics/AnalysisHelper.java
+++ src/Analytics/AnalysisHelper.java
@@ -35,14 +35,72 @@ public class AnalysisHelper {
     }
 
     public static void BestThreeCustomers() {
-        
+        Business business = Business.getBusiness();
+// Solution 2: Our 3 best customers (customers who buy about target price)  
+        System.out.println("___________________________________________________________________________________________________________________________________");
+        System.out.println("Our 3 best customers (customers who buy about target price)");
+        System.out.println("");
+        List<String> top3customerNames = null;
+        Map<String, Integer> customerProfits = new HashMap<>();
+        for (SolutionOrder order :  business.getMastersolutionorderlist().getSolutionorderlist()) {
+            int profit = (order.getSellingPrice() - order.getTargetPrice()) * (order.getQuantity());
+            String customerName = order.getCustomerName();
+            customerProfits.put(customerName, customerProfits.getOrDefault(customerName, 0) + profit);
+
+            List<String> customerNames = new ArrayList<>(customerProfits.keySet());
+
+            Collections.sort(customerNames, (a, b) -> Integer.compare(customerProfits.get(b), customerProfits.get(a)));
+            top3customerNames = customerNames.subList(0, Math.min(3, customerNames.size()));
+        }
+        for (int i = 0; i < top3customerNames.size(); i++) {
+            System.out.println(top3customerNames.get(i) + " Total Profits "
+                    + customerProfits.get(top3customerNames.get(i)));
+        }     
     }
 
     public static void BestThreeSalesPeople() {
+        Business business = Business.getBusiness();
+// Solution 3: Our top 3 best sales people (sell higher that target)   
+        System.out.println("___________________________________________________________________________________________________________________________________");
+        System.out.println("Our top 3 best sales people (sell higher that target)");
+        System.out.println("");
+        List<String> top3salesPerson = null;
+        Map<String, Integer> salesPersonProfits = new HashMap<>();
+        for (SolutionOrder order : business.getMastersolutionorderlist().getSolutionorderlist()) {
+            int profit = (order.getSellingPrice() - order.getTargetPrice()) * (order.getQuantity());
+            String salesPersonName = order.getSalesPerson();
+            salesPersonProfits.put(salesPersonName, salesPersonProfits.getOrDefault(salesPersonName, 0) + profit);
 
+            List<String> salesPersonNames = new ArrayList<>(salesPersonProfits.keySet());
+
+            Collections.sort(salesPersonNames, (a, b) -> Integer.compare(salesPersonProfits.get(b), salesPersonProfits.get(a)));
+            top3salesPerson = salesPersonNames.subList(0, Math.min(3, salesPersonNames.size()));
+        }
+        for (int i = 0; i < top3salesPerson.size(); i++) {
+            System.out.println(top3salesPerson.get(i) + " Total Profits "
+                    + salesPersonProfits.get(top3salesPerson.get(i)));
+        }
     }
 
     public static void TotalMarginalRevenueByMarket() {
+        Business business = Business.getBusiness();
+        System.out.println("___________________________________________________________________________________________________________________________________");
+        System.out.println("Our total marginal revenue broken down by market that is above or below expected target (actual minus target)");
+        System.out.println("");
+        Map<String, Integer> marketProfits = new HashMap<>();
+        List<String> marketNames = null;
+        for (SolutionOrder order : business.getMastersolutionorderlist().getSolutionorderlist()) {
+            int profit = (order.getSellingPrice() - order.getTargetPrice()) * (order.getQuantity());
+            String marketName = order.getMarketName();
+            marketProfits.put(marketName, marketProfits.getOrDefault(marketName, 0) + profit);
 
+            marketNames = new ArrayList<>(marketProfits.keySet());
+
+            Collections.sort(marketNames, (a, b) -> Integer.compare(marketProfits.get(b), marketProfits.get(a)));
+        }
+        for (int i = 0; i < marketNames.size(); i++) {
+            System.out.println(marketNames.get(i) + " Total Revenue "
+                    + marketProfits.get(marketNames.get(i)));
+        }
     }
 }
